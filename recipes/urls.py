6a085bb9320c2cd0791874bee7acd31ff75f3e87
Aleksey525 <alex_tolchin@mail.ru>
2024-11-02 18:53:08 +0300

    connected a page with a trial recipe

--- recipes/urls.py
+++ recipes/urls.py
@@ -5,4 +5,5 @@ app_name = 'recipes'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('trial_recip_page/', views.trial_recip_page, name='trial_recip_page' )
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html' )
+
+
+def trial_recip_page(request):
+    return render(request, 'trial_recip_page.html')
