614164d8bfd9004dacff2499a4d82606c9da328f
Aleksey525 <alex_tolchin@mail.ru>
2024-11-02 19:27:53 +0300

    created models for the recipes application and connected the admin panel

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
-# Register your models here.
+from .models import Recipes, Ingredient, RecipeIngredient
+
+
+class RecipeIngredientInline(admin.TabularInline):
+    model = RecipeIngredient
+    extra = 1
+
+
+@admin.register(Recipes)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'image_preview', 'publication')
+    search_fields = ('title',)
+    list_filter = ('ingredients',)
+    fields = ('title', 'description', 'image', 'image_preview', 'publication')
+    readonly_fields = ('image_preview',)
+    inlines = [
+        RecipeIngredientInline
+    ]
+
+    def image_preview(self, obj):
+        if obj.image:
+            return mark_safe(f'<img src="{obj.image.url}" style="width: 100px; height: auto;" />')
+        return "Нет изображения"
+
+    image_preview.allow_tags = True
+    image_preview.short_description = 'Предпросмотр изображения'
+
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ('title', 'calories')
+    search_fields = ('title',)
+
+
+
--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
-# Create your models here.
+
+class Recipes(models.Model):
+    PUBLICATION_CHOICES = (
+            ('Published', 'Опубликовано'),
+            ('archived', 'В архиве'),
+        )
+    title = models.CharField(max_length=200, verbose_name='Название рецепта')
+    description = models.TextField(blank=True, verbose_name="Описание рецепта")
+    ingredients = models.ManyToManyField('Ingredient', verbose_name="Ингредиенты")
+    image = models.ImageField(upload_to='media/recipes/img', verbose_name="Картинка с рецептом")
+    publication = models.CharField(max_length=20, choices=PUBLICATION_CHOICES, default='archived', verbose_name="Опубликовано")
+
+    class Meta:
+        verbose_name = "Рецепт"
+        verbose_name_plural = "Рецепты"
+
+    def __str__(self):
+        return self.title
+
+
+class Ingredient(models.Model):
+    title = models.CharField(max_length=200, verbose_name='Название ингредиента')
+    calories = models.PositiveIntegerField(blank=True, null=True, verbose_name="Калории")
+
+    class Meta:
+        verbose_name = "Ингредиент"
+        verbose_name_plural = "Ингредиенты"
+
+    def __str__(self):
+        return self.title
+
+
+class RecipeIngredient(models.Model):
+    recipe = models.ForeignKey(Recipes, on_delete=models.CASCADE)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    amount = models.IntegerField(default=0, blank=True, null=True)
+
+    class Meta:
+        verbose_name = "Ингредиент"
+        verbose_name_plural = "Ингредиенты"
+
+
